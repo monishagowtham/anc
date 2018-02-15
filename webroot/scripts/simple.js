890e2b9fa221bef84cfb60c30ce600d071f3a7af
Austin Barrett <austinbrrtt@gmail.com>
2018-02-15 02:46:02 -0500

    Switched sample data to slightly more relevant example

--- webroot/scripts/simple.js
+++ webroot/scripts/simple.js
@@ -7,17 +7,23 @@ var nodes, edges, options;
 
 function makeGraph() {
   nodes = new vis.DataSet([
-    {id: 1, label: 'Austin'},
-    {id: 2, label: 'Max'},
-    {id: 3, label: 'Kyle'},
-    {id: 4, label: 'Perogative'}
+    {id: 1, label: 'Alice'},
+    {id: 2, label: 'Bob'},
+    {id: 3, label: 'Charlie'},
+    {id: 4, label: 'David'},
+    {id: 5, label: 'Eve'}
   ])
 
   edges = new vis.DataSet([
-    {from: 1, to: 2, label: 'inGroupwith', arrows: 'to, from'},
-    {from: 2, to: 3, label: 'inGroupwith', arrows: 'to, from'},
-    {from: 3, to: 1, label: 'inGroupwith', arrows: 'to, from'},
-    {from: 4, to: 3, label: 'is his', arrows: 'to'}
+    {from: 1, to: 2, label: 'mother', arrows: 'from'},
+    {from: 2, to: 1, label: 'son', arrows: 'from'},
+    {from: 1, to: 4, label: 'mother', arrows: 'from'},
+    {from: 4, to: 1, label: 'son', arrows: 'from'},
+    {from: 2, to: 4, label: 'brother', arrows:'to,from'},
+    {from: 4, to: 5, label: 'beneficiary', arrows: 'to'},
+    {from: 4, to: 5, label: 'business partner', arrows: 'to, from'},
+    {from: 2, to: 5, label: 'friend', arrows: 'to, from'},
+    {from: 1, to: 3, label: 'acquantaince', arrows: 'to, from'}
   ])
 
   var graph = document.getElementById('graph')
