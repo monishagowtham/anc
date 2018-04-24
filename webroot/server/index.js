93256b7b1ae5f586a94c63d08d3ee264de1f95a9
Austin Barrett <austinbrrtt@gmail.com>
2018-04-23 22:47:44 -0400

    Added Node and Relationship API things

--- webroot/server/index.js
+++ webroot/server/index.js
@@ -19,6 +19,52 @@ neo4j.createConnection('neo4j', '12345', function(session) {
     return res.send(JSON.stringify({message: 'hello, world'}))
   })
 
+  app.get('/api/nodes', function (req, res) {
+    var records = []
+    session.run('MATCH (n1) RETURN n1 LIMIT 10000')
+    .subscribe({
+      onNext: function (record) {
+        var rec = {
+          type: record._fields[0].labels[0],
+          properties: record._fields[0].properties
+        }
+        records.push(rec)
+      },
+      onCompleted: function () {
+        //session.close()
+        console.log('Record is: ', records)
+        res.send(JSON.stringify({ neoRecords: records}))
+      },
+      onError: function (error) {
+        console.log(error)
+      }
+    })
+  })
+
+  app.get('/api/relationships', function (req, res) {
+    var records = []
+    session.run('MATCH (n1)-[r]->(n2) RETURN r, n1, n2 LIMIT 10000')
+    .subscribe({
+      onNext: function (record) {
+        var rec = {
+          type: record._fields[0].type,
+          properties: record._fields[0].properties,
+          from: record._fields[1].properties.name,
+          to: record._fields[2].properties.name
+        }
+        records.push(rec)
+      },
+      onCompleted: function () {
+        //session.close()
+        console.log('Record is: ', records)
+        res.send(JSON.stringify({ neoRecords: records}))
+      },
+      onError: function (error) {
+        console.log(error)
+      }
+    })
+  })
+
   app.get('/api/everything', function(req, res){
     var records = []
     session.run('MATCH (n1)-[r]->(n2) RETURN r, n1, n2 LIMIT 1000')
