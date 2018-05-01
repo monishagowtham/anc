c0db4ce54b4f757d059ac742284c6fa15bd074fa
Austin Barrett <austinbrrtt@gmail.com>
2018-04-30 21:42:20 -0400

    Refactored angular controllers

--- webroot/scripts/graphController.js
+++ webroot/scripts/graphController.js
@@ -1,36 +1,57 @@
 /**
- * Defines a simple vis.js graph for testing
- * Written by Austin Barrett
+ * Controller for the vis.js graph
+ * Written by Austin Barrett, Max Meyer, and Kyle Sturmer
  */
 
-angular.module('rtApp').controller('GraphController', ['$scope','$http', function ($scope,$http) {
+angular.module('rtApp')
+        .controller('GraphController',['$scope','$http', ($scope,$http) => {
 
+  /*
+   * Declare empty arrays to fill with info from database
+   */
 
   $scope.nodes = new vis.DataSet([])
   $scope.edges = new vis.DataSet([])
   $scope.filterRelationships = []
-  var edgeid = 0
 
+  /*
+   * Used to ensure all edge ids are unique since they are required for vis.js
+   * but not important to us
+   */
+  var edgeId = 0
+
+  /*
+   * Get all nodes from the Express API and add them to $scope.nodes to be used
+   * in the graph
+   */
   $http({
       method : "GET",
       url : "http://localhost:8005/api/nodes"
   })
   .then(function mySuccess(response) {
     response.data.neoRecords.forEach(function(record){
-        $scope.nodes.add([{id: record.properties.id.low, label: record.properties.name, hidden: false}])
+        $scope.nodes.add([{id: record.properties.id.low,
+          label: record.properties.name, hidden: false}])
     })
   },
   function myError(response) {
-      console.log("I can't believe you've done this.")
+      console.log("Failed to retrieve nodes from database")
   });
 
+  /*
+   * Get all relationships from the Express API and add them to
+   * $scope.relationships to be used in graph
+   */
   $http({
           method : "GET",
           url : "http://localhost:8005/api/relationships"
   })
   .then(function mySuccess(response) {
     response.data.neoRecords.forEach(function(record){
-      $scope.edges.add([{id: edgeid, from: record.from, to: record.to, label: record.type, arrows: 'from', hidden: false}])
+      $scope.edges.add([{id: edgeId, from: record.from, to: record.to,
+                        label: record.type, arrows: 'from', hidden: false}])
+
+      // Loop through relationships and push to filterRelationships
       var typeNew = true
       $scope.filterRelationships.forEach((filterRel) => {
         if (filterRel === record.type) {
@@ -40,35 +61,54 @@ angular.module('rtApp').controller('GraphController', ['$scope','$http', functio
       if (typeNew) {
         $scope.filterRelationships.push(record.type)
       }
-      edgeid ++ // need to send data to relationshp list
+
+      // increment edgeId so all edge ids are unique
+      edgeId ++
     });
   },
   function myError(response) {
-      console.log("I can't believe you've done this.")
+      console.log("Failed to retrieve relationships from database")
   });
 
-  console.log($scope.rawNodes)
-
+  /*
+   * Create the graph
+   */
   var graph = document.getElementById('graph')
   $scope.data = {
     nodes: $scope.nodes,
     edges: $scope.edges
   }
-  var options = {}
-  $scope.network = new vis.Network(graph, $scope.data, options)
+  $scope.options = {}
+  $scope.network = new vis.Network(graph, $scope.data, $scope.options)
 
+
+  /***************************************************************************
+   * Helper Functions
+   ***************************************************************************/
+
+  /*
+   * Function to a remove a node from the graph
+   */
   $scope.removeNode = function() {
     $scope.nodes.remove({id: document.getElementById('nodeId').value})
   }
 
+  /*
+   * This version was here too. Kept it for now in case I kept the wrong one
+   * since it still needs to be used later
+   *//*
   $scope.hideNode = function() {
     var hide = true;
     if ($scope.nodes.get(document.getElementById('nodeId').value).hidden) {
       hide = false;
     }
-    $scope.nodes.update({id: document.getElementById('nodeId').value, hidden : hide})
-  }
+    $scope.nodes.update({id: document.getElementById('nodeId').value,
+                        hidden : hide})
+  }*/
 
+  /*
+   * Function to hide an individual node on the graph
+   */
   $scope.hideNode = function() {
     var label = document.getElementById('nodeLabel').value
     var edges = $scope.nodes.get({
@@ -86,19 +126,9 @@ angular.module('rtApp').controller('GraphController', ['$scope','$http', functio
     })
   }
 
-  $scope.hideNodeCheckbox = function(label) {
-    var checked = document.getElementById('checkbox' + label).checked
-    var nodes = $scope.nodes.get({
-      filter: function (node) {
-        return (node.label == label)
-      }
-    })
-    nodes.forEach(function(node){
-      var id = node.id
-      $scope.nodes.update({id: id, hidden: !checked})
-    })
-  }
-
+  /*
+   * Function to hide an individual edge from the graph
+   */
   $scope.hideEdge = function() {
     var label = document.getElementById('edgeLabel').value
     var edges = $scope.edges.get({
@@ -116,7 +146,10 @@ angular.module('rtApp').controller('GraphController', ['$scope','$http', functio
     })
   }
 
-  $scope.hideEdgeCheckbox = function(label) {
+  /*
+   * Function to hide all relationships whose type match a checkbox
+   */
+  $scope.hideType = function(label) {
     var checked = document.getElementById('checkbox' + label).checked
     var edges = $scope.edges.get({
       filter: function (edge) {
@@ -129,6 +162,9 @@ angular.module('rtApp').controller('GraphController', ['$scope','$http', functio
     })
   }
 
+  /*
+   * Function to get first node with a given label
+   */
   $scope.getFirstNodeWithLabel = function(label) {
     var nodes = $scope.nodes.get({
       filter: function (node) {
@@ -138,6 +174,9 @@ angular.module('rtApp').controller('GraphController', ['$scope','$http', functio
     return nodes[0]
   }
 
+  /*
+   * Function to get first edge with a given label
+   */
   $scope.getFirstEdgeWithLabel = function(label) {
     var edges = $scope.edges.get({
       filter: function (edge) {
--- webroot/server/index.js
+++ webroot/server/index.js
@@ -10,7 +10,8 @@ neo4j.createConnection('neo4j', '12345', function(session) {
   app.use(bodyParser.json())
   app.use((req, res, next) => {
    res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   res.header("Access-Control-Allow-Headers",
+              "Origin, X-Requested-With, Content-Type, Accept");
    next();
  })
 
